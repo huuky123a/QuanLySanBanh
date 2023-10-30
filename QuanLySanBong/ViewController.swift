b8a9a94ee31c83612e8d18fceb78908b53e3d338
phamhuuky <huuky1234567@gmail.com>
2023-10-30 15:32:36 +0700

    Initial Commit

---
+++ QuanLySanBong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuanLySanBong
+//
+//  Created by Phai Qua Mon ios on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
